cc2067d9656d24a788c08d2e3c54edd1a9acec5d
YujiTakebuchi <takeyou.swift@gmail.com>
2023-05-20 03:58:19 +0900

    ローカルファイルからPutObjectコマンド作成用入力オブジェクト作成

--- index.mjs
+++ index.mjs
@@ -1,5 +1,47 @@
+import * as fs from "node:fs/promises";
+import dotenv from "dotenv";
+
+const createPutObjectCommandInputByLocalFile = async (
+  bucket,
+  filePath,
+  contentType,
+  fileName
+) => {
+  return fs
+    .readFile(filePath)
+    .then((f) => {
+      const input = {
+        Bucket: bucket,
+        Body: f,
+        ContentType: contentType,
+        Key: fileName,
+      };
+      return input;
+    })
+    .catch((err) => {
+      const errorMessage = "Failed to read local file...";
+      console.error(errorMessage);
+      console.error(err);
+      const responseJson = JSON.stringify({
+        statusCode: 400,
+        body: {
+          errorMessage,
+        },
+      });
+      throw new Error(responseJson);
+    });
+};
+
 export const handler = async (event, context, callback) => {
-  return fs.readFile("./uploads/metal-gear-solid-jamming.gif").then((f) => {
-    console.log(f);
+  dotenv.config();
+  const env = process.env;
+  const bucket = env.S3_BUCKET_NAME;
+  return createPutObjectCommandInputByLocalFile(
+    bucket,
+    "./uploads/metal-gear-solid-jamming.gif",
+    "image/gif",
+    "metal-gear-solid-jamming.gif"
+  ).then((data) => {
+    console.log(data);
   });
 };
