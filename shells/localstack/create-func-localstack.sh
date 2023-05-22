8a58888c3e2d7e2ff46b594464105b8a371277d1
YujiTakebuchi <takeyou.swift@gmail.com>
2023-05-23 01:43:22 +0900

    元ディレクトリ構成に合わせてzipファイル作成するシェルスクリプトから不要なファイルを除外

--- shells/localstack/create-func-localstack.sh
+++ shells/localstack/create-func-localstack.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-zip -r app.zip . -x "Dockerfile" "package*.json" "*.log" "*.yml" "*.sh"
+zip -r app.zip . -x "Dockerfile" "package*.json" "*.log" "*.yml" "shells/*" ".git/*" ".gitignore"
 awslocal lambda create-function --region ap-northeast-1 --function-name s3-lambda --zip-file fileb://app.zip --role arn:aws:iam::000000000000:role/lambda-role --handler index.handler --runtime nodejs18.x
